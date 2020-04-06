7f5296af98b65fd0803746990488c6b17180c890
Jose Juan Suarez Elizalde <59002673+JoseJuanSE@users.noreply.github.com>
2020-04-06 10:00:43 -0500

    Add files via upload
    Template to program faster in contests (less typing)

---
+++ Template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+#define endl '\n'
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define ALL(v) v.begin(), v.end()
+#define deb cout<<"HOLA\n";
+#define UNIQUE(v) (v).resize(unique(ALL(v))-(v).begin())
+#define forn(i,n) for (int i = 0; i < n; i++)
+#define forr(i,a,b) for (int i = a; i <= b; i++)
+#define bs(v,f) binary_search(v.begin(),v.end(),f)
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string str;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef pair<ii, int> iii;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<ll> vl;
+typedef vector<ii> vii;
+
+int main(){
+	
+	return 0;
+}
+
