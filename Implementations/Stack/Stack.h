c5c3f09156810b1bd06aafefd19c2a3a90918572
Jose Juan Suarez Elizalde <59002673+JoseJuanSE@users.noreply.github.com>
2020-04-07 11:19:02 -0500

    Update Stack.h

--- Implementations/Stack/Stack.h
+++ Implementations/Stack/Stack.h
@@ -12,17 +12,17 @@ Stack Push(Elem x,Stack res){
     nuevo->next=res;
     return nuevo;
 }
-/*--Espec Pila
-empty:->Pila;
-push:elem,pila->pila;
-isempty:Pila->Bool;
-top:Pila->Elem;
-pop:Pila->Pila;
+/*--Specs Stack
+Empty:->Pila;
+Push:elem,pila->pila;
+IsEmpty:Pila->Bool;
+Top:Pila->Elem;
+Pop:Pila->Pila;
 
---Axiomas: Elem e,Pila p;
-[p1]	isempty(empty())=Cierto;
-[p2]	isempty(push(e,p))=false;
-[p3]	top(empty())->Error
-[p4]	top(push(e,p))=e;
-[p5]	pop(empty())->Error
-[p6]	pop(push(e,p))=p;*/
+--Axioms: Elem e,Pila p;
+[p1]	IsEmpty(empty())=Cierto;
+[p2]	isEmpty(push(e,p))=false;
+[p3]	Top(empty())->Error
+[p4]	Top(push(e,p))=e;
+[p5]	Pop(empty())->Error
+[p6]	Pop(push(e,p))=p;*/
