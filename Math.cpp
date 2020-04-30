d186a88e13b7fb30bed5a85c865053c7c36c62eb
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2020-04-30 16:57:18 -0500

    Create Math.cpp

---
+++ Math.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+ull b_e(ull n,ull k){
+	ull x=1;
+	while(k){
+		if(k&1)
+			x*=n;
+		n*=n;
+		k>>=1;
+	}
+	return x;
+}
+int main(){
+	ull n,m;
+	cin>>n>>m;
+	cout<<b_e(n,m);
+	return 0;
+}
