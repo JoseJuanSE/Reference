ae40c18fa1e8e559fff0ca80e095ef0e38ead73b
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2020-04-30 18:20:06 -0500

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 typedef unsigned long long ull;
 //Binary Exponentiation
 ull b_e(ull n,ull k){
@@ -12,9 +13,16 @@ ull b_e(ull n,ull k){
 	}
 	return x;
 }
+bool Is_Prime(long long x){
+	if(x%2==0)return false;
+	for(int i=3;i*i<=x;i+=2)
+		if(x%i==0)return false;
+	return true;
+}
 int main(){
 	ull n,m;
 	cin>>n>>m;
-	cout<<b_e(n,m);
+	cout<<b_e(n,m)<<endl;
+	cout<<Is_Prime(n);
 	return 0;
 }
