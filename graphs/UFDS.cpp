91c0cda01a4f3798b1588792e204b712f9ede30b
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2020-10-26 17:23:48 -0600

    Update UFDS.cpp

--- graphs/UFDS.cpp
+++ graphs/UFDS.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 //Union Find Disjoint Set
 struct UFDS{
-	vector<int>p,s;
+	vector<int>p,r;
 	int ns;
 	UFDS(int n){
 		ns=n;
 		n++;
 		p.assign(n,0);
-		s.assign(n,1);
+		r.assign(n,1);
 		for(int i=0;i<n;i++)p[i]=i;
 	}
 	int FindSet(int i){return p[i]==i?i:p[i]=FindSet(p[i]);}
 	bool IsSameSet(int i,int j){return FindSet(i)==FindSet(j);}
-	int Size(int i){return s[FindSet(i)];}
 	int NumSet(){return ns;}
 	bool UnionSet(int i,int j){
 		int x=FindSet(i),y=FindSet(j);
 		if(x==y)
 		 	return false;
-		if(s[x]<s[y])
+		ns--;
+		if(r[x]<r[y])
 			swap(x,y);
-		if(s[x]==s[y])
-			s[x]++;
+		if(r[x]==r[y])
+			r[x]++;
 		p[y]=x;
 		return true;
 	}
