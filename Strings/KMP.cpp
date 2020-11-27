15777e38b21dc1f1479690325313f1e8c0932543
JoseJuanSE <jojusuarez46@gmail.com>
2020-11-27 16:54:13 -0600

    kmp working

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 void KMP(string a,string b){
     vector<int> lps(a.size(),0);
-    int beg=0;
+    int beg=0,j=0;
     for(int i=1;i<a.size();i++){
         if(a[beg]==a[i]){beg++;lps[i]=beg;}
         else if(beg!=0){beg = lps[beg-1];i--;}
     }
     for(int i=0;i<b.size();i++){
-        if(a[0]==b[i])
-        for(int j=0;j<a.size();j++){
-            
-            if(j-i+1==a.size())cout<<
+        if(a[j]==b[i]){
+            j++;
+            if(j==a.size()){
+                j--;
+                cout<<i-a.size()+1<<endl;
+                bool f = true;
+                while(j!=0 and (a[j]!=b[i] or f)){
+                    j = lps[j-1];
+                    f=false;
+                }
+                i--;
+            }
+        }else{
+            while(j!=0 and a[j]!=b[i]) j = lps[j-1];
         }
     }
 }
