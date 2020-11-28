7439abd00419e2abf31c3973124876ae9de53eaf
JoseJuanSE <jojusuarez46@gmail.com>
2020-11-27 18:25:40 -0600

    all fixed

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void KMP(string a,string b){
+void KMP(string &a,string &b){
     vector<int> lps(a.size(),0);
     int beg=0,j=0;
     for(int i=1;i<a.size();i++){
@@ -8,11 +8,12 @@ void KMP(string a,string b){
         else if(beg!=0){beg = lps[beg-1];i--;}
     }
     for(int i=0;i<b.size();i++){
+        //cout<<"J: "<<j<<" A: "<<a[j]<<" I: "<<i<<" B: "<<b[i]<<endl;
         if(a[j]==b[i]){
             j++;
             if(j==a.size()){
                 j--;
-                cout<<i-a.size()+1<<endl;
+                cout<<i-a.size()+1<<" ";
                 bool f = true;
                 while(j!=0 and (a[j]!=b[i] or f)){
                     j = lps[j-1];
@@ -21,7 +22,9 @@ void KMP(string a,string b){
                 i--;
             }
         }else{
-            while(j!=0 and a[j]!=b[i]) j = lps[j-1];
+            bool f = false;
+            while(j!=0 and a[j]!=b[i]){ j = lps[j-1];f=true;}
+            if(f)i--;
         }
     }
 }
