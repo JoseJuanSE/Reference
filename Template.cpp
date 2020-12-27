fe0e035ee9c5d42a846b6cea0899ecf9f07fe2bd
JoseJuanSE <jojusuarez46@gmail.com>
2020-12-26 20:46:47 -0600

    template updated

--- Template.cpp
+++ Template.cpp
@@ -6,28 +6,70 @@
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);
 #define ALL(v) v.begin(), v.end()
 #define ALLR(v) v.rbegin(), v.rend()
-#define deb cout<<"HOLA\n";
 #define UNIQUE(v) (v).resize(unique(ALL(v))-(v).begin())
 #define forn(i,n) for (int i = 0; i < n; i++)
-#define forr(i,a,b) for (int i = a; i <= b; i++)
-#define bsc(v,f) binary_search(ALL(v),f)
+#define forr(i, a, b) for(int i=(int)a;i <= (int)b;i++)
+#define forri(i, b, a) for(int i=(int)b; i >= (int)a;i--)
+#define bse(v,f) binary_search(ALL(v),f)
 #define lb(v,f) lower_bound(ALL(v),f)
 #define ub(v,f) upper_bound(ALL(v),f)
 #define pub push_back
-#define OJ freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 
 using namespace std;
 
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " = " << a << endl;
-	err(++it, args...);
+//--------------DEBUG--------------
+#define ts to_string
+string ts(char c) { return string(1, c); }
+string ts(bool b) { return b ? "true" : "false"; }
+string ts(const char* s) { return (string)s; }
+string ts(string s) { return s; }
+template<class A> string ts(complex<A> c) { 
+	stringstream ss; ss << c; return ss.str(); }
+string ts(vector<bool> v) { 
+	string res = "{"; for(int i = 0; i < (int)(v).size(); ++i) res += char('0' + v[i]);
+	res += "}"; return res; }
+template<size_t SZ> string ts(bitset<SZ> b) {
+	string res = ""; for(int i = 0; i < SZ; ++i) res += char('0' + b[i]);
+	return res; }
+template<class A, class B> string ts(pair<A,B> p);
+template<class T> string ts(T v) { // containers with begin(), end()
+	bool fst = 1; string res = "{";
+	for (const auto& x: v) {
+		if (!fst) res += ", ";
+		fst = 0; res += ts(x);
+	}
+	res += "}"; return res;
 }
+template<class A, class B> string ts(pair<A,B> p) {
+	return "(" + ts(p.fi) + ", " + ts(p.se) + ")"; }
+
+void DBG() { cerr << "]" << endl; }
+template<class H, class... T> void DBG(H h, T... t) {
+	cerr << ts(h); if (sizeof...(t)) cerr << ", ";
+	DBG(t...); }
+void EDBG(string names) { names = names; }
+template<class H, class... T> void EDBG(string names, H h, T... t) {
+	auto pos = names.find(',');
+	auto first_name = names.substr(0, pos);
+	auto rest = names.substr(pos + 1);
+	while(rest.front() == ' '){
+		rest = rest.substr(1);
+	}
+	cerr << "[" << first_name << "]: [" << ts(h) << "]" << "\n";
+	EDBG(rest, t...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define edbg(...) EDBG(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 0
+#define edbg(...) 0
+#endif
+//--------------DEBUG--------------
 
 typedef long long ll;
 typedef unsigned long long ull;
+typedef long double ld;
 typedef string str;
 typedef pair<int, int> ii;
 typedef pair<ii, int> iii;
@@ -37,10 +79,15 @@ typedef vector<ll> vl;
 typedef vector<ii> vii;
 
 void solve(){
-    
+    ll n,m,x;
+    cin>>n>>m;
+    vi a(n);
+    forn(i,n){
+        cin>>a[i];
+    }
 }
 int main(){
-    //OJ;
+    fast
     int t=1;
     cin>>t;
     while(t--)
